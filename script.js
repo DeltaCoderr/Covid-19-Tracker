4d4b5bcd3e67d49bec4cd4941083057320e25ccd
DeltaCoderr <51528076+DeltaCoderr@users.noreply.github.com>
2020-07-26 15:26:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ let btn = document.getElementById("btn");
                         break;
                     }
                 }
+                    
                 let data = document.querySelector(".data");
                 data.innerHTML = `<div class="box">
                                 <div class="head">
