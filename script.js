918988823d97e2c56081b5e095703476227ba188
Ravi Ujjwal <ravi.ujjwal2000@gmail.com>
2020-10-01 02:00:53 +0530

    Made many small changes in the HTML/CSS/JS codes. Improved the JS code so that it loads fast by adding variables. Also, added new sections namely ActiveTotal, Fatalities (%), Recovery (%) Added some hovering effect using CSS. Also, added a loading spinner which upon clicking search spins.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let btn = document.getElementById("btn");
-        
+
         btn.addEventListener("click",()=>{
         let text = document.getElementById("getText").value;
+        document.getElementById("loader").innerHTML = "<i class='fa fa-spinner fa-spin' style='font-size:31px'></i>";
         //Credits to the API
             fetch('https://api.covid19api.com/summary')
             .then((covidData)=>{
@@ -9,38 +10,49 @@ let btn = document.getElementById("btn");
             })
            //Fetches the Information from the API in JSON format
             .then((getData)=>{
+              document.getElementById("loader").innerHTML = "";
                 console.log(getData);
-                var content = document.querySelector(".data"); 
+                var content = document.querySelector(".data");
 
-                var box = content.lastElementChild;  
-                while (box) { 
-                    content.removeChild(box); 
-                    box = content.lastElementChild; 
-                } 
+                var box = content.lastElementChild;
+                while (box) {
+                    content.removeChild(box);
+                    box = content.lastElementChild;
+                }
 
                 var index = 0;
-              //Loop for testing purposes.     
-                for(var i=0;i<185;i++){
+                var totalCountries = getData.Countries.length;
+              //Loop for testing purposes.
+                for(var i=0;i<totalCountries ;i++){
                     if(getData.Countries[i].Country.toLowerCase() == text.toLowerCase()){
                         index = i;
                         break;
                     }
                 }
                     //HTML starts here
+                let countryData = getData.Countries[index];
+                let percentRecovered = (countryData.TotalRecovered/countryData.TotalConfirmed)*100;
+                let percentFatalities = (countryData.TotalDeaths/countryData.TotalConfirmed)*100;
+                let totalActive = countryData.TotalConfirmed-(countryData.TotalRecovered+countryData.TotalDeaths);
                 let data = document.querySelector(".data");
                 data.innerHTML = `<div class="box">
                                 <div class="head">
-                                    <span>Covid-19 Cases in ${getData.Countries[index].Country}</span>
+                                    <span>Covid-19 Cases in ${countryData.Country}</span>
                                 </div>
                                 <div class="total">
-                                    <div><p>TotalConfirmed</p> ${getData.Countries[index].TotalConfirmed}</div>
-                                    <div><p>TotalDeaths</p> ${getData.Countries[index].TotalDeaths}</div>
-                                    <div><p>TotalRecovered</p> ${getData.Countries[index].TotalRecovered}</div>
+                                    <div><p>TotalConfirmed</p> ${countryData.TotalConfirmed}</div>
+                                    <div><p>TotalDeaths</p> ${countryData.TotalDeaths}</div>
+                                    <div><p>TotalRecovered</p> ${countryData.TotalRecovered}</div>
                                 </div>
+                                <div class="percent">
+                                    <div><p>TotalActive</p> ${totalActive}</div>
+                                    <div><p>Fatalities (%)</p> ${percentFatalities.toFixed(2)}</div>
+                                    <div><p>Recovered (%)</p> ${percentRecovered.toFixed(2)}</div>
+                                    </div>
                                 <div class="new">
-                                    <div><p>NewConfirmed</p> ${getData.Countries[index].NewConfirmed}</div>
-                                    <div><p>NewDeaths</p> ${getData.Countries[index].NewDeaths}</div>
-                                    <div><p>NewRecovered</p> ${getData.Countries[index].NewRecovered}</div>
+                                    <div><p>NewConfirmed</p> ${countryData.NewConfirmed}</div>
+                                    <div><p>NewDeaths</p> ${countryData.NewDeaths}</div>
+                                    <div><p>NewRecovered</p> ${countryData.NewRecovered}</div>
                                     </div>
                                 </div>`;
             })
